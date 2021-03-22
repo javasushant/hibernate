3658b17c45332b652e63d26c4396387727cc8b5b
TL <TL"gmail.com>
2021-03-22 17:22:18 +0530

    HB Proj 1

---
+++ HBProj1-SaveObject/src/com/nt/entity/Actor.java
@@ -0,0 +1,51 @@
+package com.nt.entity;
+
+public class Actor {
+	private int actorid;
+	private String actorName;
+	private String actorAddrs;
+	private long phone;
+	private float renumeration;
+	
+	//
+	public int getActorid() {
+		return actorid;
+	}
+	public void setActorid(int actorid) {
+		this.actorid = actorid;
+	}
+	public String getActorName() {
+		return actorName;
+	}
+	public void setActorName(String actorName) {
+		this.actorName = actorName;
+	}
+	public String getActorAddrs() {
+		return actorAddrs;
+	}
+	public void setActorAddrs(String actorAddrs) {
+		this.actorAddrs = actorAddrs;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public float getRenumeration() {
+		return renumeration;
+	}
+	public void setRenumeration(float renumeration) {
+		this.renumeration = renumeration;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Actor [actorid=" + actorid + ", actorName=" + actorName + ", actorAddrs=" + actorAddrs + ", phone="
+				+ phone + ", renumeration=" + renumeration + "]";
+	}
+	
+	
+	
+}
